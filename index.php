cd9e8b5854eee42998922b94b303d8f1f3eca28c
bmatlik <106585052+bmatlik@users.noreply.github.com>
2022-06-17 22:59:23 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,42 @@
+<title>Avaleht</title>
+
+<?php $page = 'home';include 'navbar.php' ?>
+
+<style>
+
+h1{
+	text-align: center;
+	font-size: 3em !important;
+}
+p {
+	text-align: center;
+	padding-left: 20px;
+	padding-right: 20px;
+
+}
+
+
+
+</style>
+
+</head>
+
+<body>
+
+<div class="container">
+	<h1>See on meie avaleht</h1>
+	<p>saatke meile taotlus</p>
+	<p>Kui midagi vaja on.</p>
+	<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+	<p>................</p>
+	<p>................</p>
+	<p>................</p>
+	<p>................</p>
+	&nbsp;
+</div>
+
+<?php include "footer.php" ?>
+
+</body>
+
+</html>
