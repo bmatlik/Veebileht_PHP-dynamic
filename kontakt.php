7b826be824da338ca854381b588a2a21cfbdb03a
bmatlik <106585052+bmatlik@users.noreply.github.com>
2022-06-18 12:11:24 +0300

    Update kontakt.php

--- kontakt.php
+++ kontakt.php
@@ -33,8 +33,7 @@ p {
 <div class="container">
 	<h1>Info</h1>
 </div>
-	&nbsp;
-<p><iframe class="maps" width="600" height="350" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://www.openstreetmap.org/export/embed.html?bbox=23.930969238281254%2C59.206173075493126%2C25.216369628906254%2C59.66774058164963&amp;layer=mapnik" style="border: 0px solid black"></iframe><br/><small><a href="https://www.openstreetmap.org/#map=10/59.4377/24.5737">Kuvage suuremat kaarti</a></small></p>
+<p><iframe class="maps" width="600" height="350" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://www.openstreetmap.org/export/embed.html?bbox=23.930969238281254%2C59.206173075493126%2C25.216369628906254%2C59.66774058164963&amp;layer=mapnik" style="border: 0px solid black"></iframe> &nbsp;<br/><small><a href="https://www.openstreetmap.org/#map=10/59.4377/24.5737">Kuvage suuremat kaarti</a></small></p>
 
 &nbsp;
 
